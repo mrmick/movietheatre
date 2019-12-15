dd04f6ccae1342cad8a7bb8a7df83cd5de4d4565
mrmick <mrmickca@gmail.com>
2019-12-15 17:34:36 -0500

    updated model unit tests

--- theatre/tests.py
+++ theatre/tests.py
@@ -28,6 +28,7 @@ class MovieTests(TestCase):
         self.assertIsInstance(self.movie, Movie)
         self.assertEqual(self.movie.title, 'Star Wars')
 
+
 class ShowingTests(TestCase):
     def setUp(self):
         self.room = Room.objects.create(name="Jaba Room", seats_capacity=100)
@@ -39,4 +40,29 @@ class ShowingTests(TestCase):
         )
 
     def test_str(self):
-        self.assertEqual(str(self.showing), 'Showing for %s in %s at %s' % (self.movie.title, self.room.name, self.showing.showtime))
+        self.assertEqual(str(self.showing),
+                         'Showing for %s in %s at %s' % (self.movie.title, self.room.name, self.showing.showtime))
+
+    def test_available_seats_at_start(self):
+        self.assertEqual(self.showing.available_seats(), self.showing.room.seats_capacity)
+
+    def test_seats_available_at_start_with_requested_seats(self):
+        self.assertTrue(self.showing.seats_available(requested=1))
+        self.assertTrue(self.showing.seats_available(requested=50))
+        self.assertTrue(self.showing.seats_available(requested=99))
+        self.assertTrue(self.showing.seats_available(requested=100))
+        # what if we have more than the initial available seats?
+        self.assertFalse(self.showing.seats_available(requested=101))
+
+    def test_seats_available_at_start(self):
+        self.assertTrue(self.showing.seats_available())
+
+    def test_available_seats_after_selling_some(self):
+        self.assertEqual(self.showing.available_seats(), self.showing.room.seats_capacity)
+        self.showing.sold_seats = 50
+        self.assertEqual(self.showing.available_seats(), self.showing.room.seats_capacity - 50)
+        # Make sure we can still see seats as available for purchase
+        self.assertTrue(self.showing.seats_available())
+        self.assertTrue(self.showing.seats_available(requested=20))
+        self.assertTrue(self.showing.seats_available(requested=50))
+        self.assertFalse(self.showing.seats_available(requested=51))
